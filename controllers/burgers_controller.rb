2d0667329a8002a07f3542f5cd35188efab93395
Eugene Kim <eugenekim.edi@gmail.com>
2017-06-21 10:57:57 +0100

    added burger:new in controller

--- controllers/burgers_controller.rb
+++ controllers/burgers_controller.rb
@@ -1,9 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/burger.rb')
+require_relative('../models/restaurant.rb')
 
 get '/burgers' do
   @burgers = Burger.all()
   erb ( :"burgers/index")
 end
 
+get '/burgers/new' do
+  @restaurants =  Restaurant.all()
+  erb (:"burgers/new")
+end
